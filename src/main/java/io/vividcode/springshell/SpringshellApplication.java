8b85e1f39e22b428e16f1106ee7ec442a474908e
Fu Cheng <alexcheng1982@gmail.com>
2017-09-29 15:35:53 +1300

    basic code

---
+++ src/main/java/io/vividcode/springshell/SpringshellApplication.java
@@ -0,0 +1,12 @@
+package io.vividcode.springshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringshellApplication.class, args);
+	}
+}
---
+++ src/test/java/io/vividcode/springshell/SpringshellApplicationTests.java
@@ -0,0 +1,16 @@
+package io.vividcode.springshell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringshellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
